39ee90a3b32b211099d7006c56fc7931b86a13fb
chenwenzhuo <Chenwz1996@163.com>
2018-12-08 16:34:08 +0800

    添加了 Spring_Boot_Hello_World

---
+++ Spring_Boot_Hello_World/src/main/java/com/heythere/Spring_Boot_Hello_World/SpringBootHelloWorldApplication.java
@@ -0,0 +1,12 @@
+package com.heythere.Spring_Boot_Hello_World;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
+	}
+}
